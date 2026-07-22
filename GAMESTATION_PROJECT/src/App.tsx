cf63bfcfae16e48b19266519bac7119592cfe281
EduArita-coder <eduardoj1109@gmail.com>
2026-07-21 23:17:36 -0600

    Acoplando el estilo de la aplicacion a lo que va a tratar el proyecto

--- GAMESTATION_PROJECT/src/App.tsx
+++ GAMESTATION_PROJECT/src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
+import appLogo from '/gamestation-icon.svg'
 import PWABadge from './PWABadge.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://www.youtube.com" target="_blank">
           <img src={appLogo} className="logo" alt="GAMESTATION logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>GAMESTATION</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='texto-main'>GAMESTATION</h1>
       <PWABadge />
     </>
   )
