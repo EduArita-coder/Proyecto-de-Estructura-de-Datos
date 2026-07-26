025c14ca38fe11fc2cb89b16cebe9a3074fc97fc
qndree <danisuarezclaudino013@gmail.com>
2026-07-25 23:52:19 -0600

    Creación de vistas base Home y Games y enrutamiento inicial

--- GAMESTATION_PROJECT/src/router/AppRouter.tsx
+++ GAMESTATION_PROJECT/src/router/AppRouter.tsx
@@ -1,12 +1,26 @@
-import { Navbar } from "../presentation/components"
+import { Route, Routes } from "react-router";
+import { Navbar } from "../presentation/components";
+import { HomeView, GamesView } from "../presentation/views";
 
 export const AppRouter = () => {
     return (
         <div className="relative min-h-screen overflow-hidden text-white">
-            <div className="pointer-events-none absolute inset-0 -z-10 bg-[url('/gamestation-fondo.png')] bg-cover bg-center opacity-30 blur-[3px] motion-safe:animate-pulse" />
-            <div className="relative z-10">
+            {/* Fondo de la aplicación */}
+            <div className="pointer-events-none absolute inset-0 -z-10 bg-[url('/gamestation-fondo.png')] bg-cover bg-center opacity-30 blur-[3px]" />
+            
+            {/* Contenido enrutado */}
+            <div className="relative z-10 flex flex-col min-h-screen">
                 <Navbar />
+                
+                <main className="flex-grow">
+                    <Routes>
+                        <Route path="/" element={<HomeView />} />
+                        <Route path="/games" element={<GamesView />} />
+                        {/* Redireccionar cualquier ruta desconocida al Home */}
+                        <Route path="*" element={<HomeView />} />
+                    </Routes>
+                </main>
             </div>
         </div>
-    )
-}
+    );
+};
